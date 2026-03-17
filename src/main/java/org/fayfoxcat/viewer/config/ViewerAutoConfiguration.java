7fddd9a4c870b69a391bd725bb1e8ab853bbf996
wsy <sywang@wiscom.com.cn>
2026-03-17 13:11:42 +0800

    fix bug

--- src/main/java/org/fayfoxcat/viewer/config/ViewerAutoConfiguration.java
+++ src/main/java/org/fayfoxcat/viewer/config/ViewerAutoConfiguration.java
@@ -7,15 +7,26 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.PropertySourceFactory;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Properties;
 
 /**
  * Viewer文件查看器自动配置类
  * 当应用为 Web 应用时自动启用文件查看器功能
+ * 支持自动加载所有 patterns*.yml 配置文件
  *
  * @author fayfoxcat
  * @version 0.0.1
@@ -30,25 +41,40 @@ public class ViewerAutoConfiguration {
     /**
      * YAML 属性源工厂
      * 用于支持 @PropertySource 注解加载 YAML 配置文件
+     * 支持加载多个 patterns*.yml 文件
      */
     public static class YamlPropertySourceFactory implements PropertySourceFactory {
 
         @Override
         @NonNull
-        public PropertiesPropertySource createPropertySource(String name, @NonNull EncodedResource resource) {
+        @SuppressWarnings("null")
+        public PropertiesPropertySource createPropertySource(@Nullable String name, @NonNull EncodedResource resource) {
+            // 尝试加载所有 patterns*.yml 文件
+            List<Resource> allResources = new ArrayList<>();
+
+            try {
+                // 搜索 classpath 下所有 patterns*.yml 文件
+                ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+                Resource[] resources = resolver.getResources("classpath*:patterns*.yml");
+
+                if (resources.length > 0) {
+                    allResources.addAll(Arrays.asList(resources));
+                } else {
+                    allResources.add(resource.getResource());
+                }
+            } catch (Exception e) {
+                allResources.add(resource.getResource());
+            }
+
             YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
-            factory.setResources(resource.getResource());
+            Resource[] resourceArray = allResources.toArray(new Resource[0]);
+            factory.setResources(resourceArray);
 
             Properties properties = factory.getObject();
             if (properties == null) {
                 properties = new Properties();
             }
-
-            String sourceName = name != null ? name : resource.getResource().getFilename();
-            if (sourceName == null) {
-                sourceName = "yamlPropertySource";
-            }
-
+            String sourceName = name != null ? name : "patterns";
             return new PropertiesPropertySource(sourceName, properties);
         }
     }
